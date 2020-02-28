f51fef1759bb536ccfa31ebef38082bfb3bc596d
jaco60 <eric.jacoboni@gmail.com>
2020-02-28 16:50:13 +0100

    Add comments

--- strutil.go
+++ strutil.go
@@ -1,5 +1,7 @@
+// strutil is a collection of functions on strings
 package strutil
 
+// Reverse returns a reversed version of its argument
 func Reverse(in string) string {
 	out := []rune(in)
 	for i, j := 0, len(out)-1; i < j; i, j = i+1, j-1 {
